fb4c720dd2c3b99828e28aec71989e5e035636c1
Michael Erastus <meritinfosllc@gmail.com>
2022-04-11 11:34:30 +0100

    Nextropay PHP Library

--- src/NextropayAPI.php
+++ src/NextropayAPI.php
@@ -1,7 +1,220 @@
 <?php
+namespace Officialmeritinfos\NextropayPhp;
 
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Officialmeritinfos\NextropayPhp\NextropayCurlRequests;
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * Nextropay PHP API Wrapper
+ *
+ * @link https://doc.nextropay.com/ Official Nextropay API Documentation.
+ * @copyright (c) 2022, Nextropay.com
+ *
+ * Many API commands require a currency or currencies values to be passed. These are always in the
+ * format of a currency ticker code. See https://doc.nextropay.com/supported-coins for these ticker codes,
+ * located in the CODE column.
+ *
+ * When developing on Sandbox, ensure that the Env is set to TEST and switch over to LIVE when live.
+ *
+ * Always send your secret key as empty string when performing actions not Payout. We rely on the empty
+ * string sent to determine if your secret key is needed.
+ */
 class NextropayAPI
 {
-    private $secretKey = '';
-    private $publicKey = '';
+    private $private_key = '';
+    private $public_key = '';
+    private $env;
+    private $request_handler;
+
+    /**
+     * @param $public_key
+     * @param $env
+     * @param $private_key
+     */
+    public function __construct($public_key,$env,$private_key='')
+    {
+        // Set keys and format passed to class
+        $this->private_key = $private_key;
+        $this->public_key = $public_key;
+        $this->env = $env;
+        // Throw an error if the keys are not both passed
+        try {
+            if (empty($this->public_key)) {
+                throw new Exception("Your secret and public keys are not both set!");
+            }
+        } catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+        // Initiate a cURL request object
+        $this->request_handler = new NextropayCurlRequests($this->public_key,$this->env,$this->private_key);
+    }
+
+    /**
+     * function CheckApiKey
+     * Get basic account information.
+     *
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function CheckApiKey()
+    {
+        return $this->request_handler->postRequests('check-key');
+    }
+    /**
+     * function GetBusiness
+     * Get Business information.
+     *
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function GetBusiness($id)
+    {
+        return $this->request_handler->getRequests('account/business/'.$id);
+    }
+    /**
+     * function UpdateBusiness
+     * Updates Business information.
+     *
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function UpdateBusiness($data)
+    {
+        return $this->request_handler->putRequests('account/business/update',$data);
+    }
+    /**
+     * function CreateInvoice
+     * Creates Invoice for payment
+     *
+     * @param $data
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function CreateInvoice($data)
+    {
+        return $this->request_handler->postRequests('invoice/create',$data);
+    }
+    /**
+     * function GetInvoice
+     * Retrieves Invoice details already created above
+     * @param $id
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function GetInvoice($id)
+    {
+        return $this->request_handler->getRequests('invoice/'.$id.'/details');
+    }
+    /**
+     * function UpdateInvoice
+     * Updates the Invoice status from Partial payment to completed
+     * @param $id
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function UpdateInvoice($id)
+    {
+        return $this->request_handler->putRequests('invoice/'.$id.'/update');
+    }
+    /**
+     * function GetInvoiceSettlementStatus
+     * Use this function to check the status of the invoice settlement
+     * Unfortunately, this is only available for White Label
+     * @param $id
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function GetInvoiceSettlementStatus($id)
+    {
+        return $this->request_handler->getRequests('invoice/'.$id.'/settlement');
+    }
+    /**
+     * function CreatePayout
+     * Creates Payouts/withdrawal
+     *
+     * @param $data
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function CreatePayout($data)
+    {
+        return $this->request_handler->postRequests('transfer/create',$data);
+    }
+    /**
+     * function GetPayout
+     * Retrieves Payout/Withdrawal details already created above
+     * @param $id
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function GetPayout($id)
+    {
+        return $this->request_handler->getRequests('invoice/'.$id.'/details');
+    }
+
+
+    /** ------------------------------------------------ **/
+    /** ------------ Miscellaneous Endpoint ------------ **/
+    /** ------------------------------------------------ **/
+
+
+    /**
+     * function GetAccountBalance
+     * Retrieves the Balance of your account before initiating payouts
+     * @param $asset
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function GetAccountBalance($asset)
+    {
+        return $this->request_handler->getRequests('account-balance/'.$asset);
+    }
+
+    /**
+     * function Rate
+     * Get the system's exchange rate
+     * @param $asset
+     * @param $fiat
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function Rate($asset,$fiat='USD')
+    {
+        return $this->request_handler->getRequests('exchange-rate/'.$asset.'/'.$fiat);
+    }
+
+    /**
+     * function GetBalance
+     * Returns a collection of all account balance
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function GetBalance()
+    {
+        return $this->request_handler->postRequests('account-balance');
+    }
+
+    /**
+     * function SupportedAssets
+     * Returns a collection of all supported crypto assets
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function SupportedAssets()
+    {
+        return $this->request_handler->getRequests('supported-assets');
+    }
+
+    /**
+     * function SupportedFiats
+     * Returns a collection of all supported local currencies
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function SupportedFiats()
+    {
+        return $this->request_handler->getRequests('supported-fiats');
+    }
 }
--- src/NextropayCurlRequests.php
+++ src/NextropayCurlRequests.php
@@ -1,9 +1,87 @@
 <?php
+namespace Officialmeritinfos\NextropayPhp;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\ClientException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
 class NextropayCurlRequests
 {
     private $secretKey = '';
     private $publicKey = '';
-    private $sandboxUrl = '';
-    private $liveUrl = '';
+    private $sandboxUrl = 'https://sandbox.nextropay.com/api/';
+    private $liveUrl = 'https://nextropay.com/api/';
+    private $url;
+    private $client;
+    private $headers;
+
+    /**
+     * Executes a cURL request to the Nextropay.com API
+     * We are using the GuzzleHttp Client package to send these requests
+     *
+     * @param $publicKey  The public key of the merchant - sandbox or live
+     * @param $env        The environment the of the application - LIVE or TEST
+     * @param $secretKey  The Secret key of the merchant. Only needed for payouts
+     */
+    public function __construct($publicKey,$env,$secretKey='')
+    {
+        $this->secretKey = $secretKey;
+        $this->publicKey = $publicKey;
+        $this->url = ($env =='LIVE') ? $this->liveUrl:$this->sandboxUrl;
+        $this->client = new Client(['base_uri'=>$this->url]);
+        $this->headers = empty($this->secretKey)? ['x-api-key'=>$this->publicKey,'Content-Type'=> 'application/json']:['x-api-sec'=>$this->secretKey,'Content-Type'=> 'application/json'];
+    }
+
+    /**
+     * @param $url
+     * @param $data
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function getRequests($url)
+    {
+        try {
+            $request = $this->client->request('GET',$this->url.$url,['headers'=>$this->headers]);
+            $response = $request->getBody();
+        } catch (ClientException $e) {
+            $response = Psr7\Message::toString($e->getResponse());
+        }
+        return $response;
+    }
+    /**
+     * @param $url
+     * @param $data
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function postRequests($url,$data='')
+    {
+        try {
+            $request = $this->client->request('POST',$this->url.$url,['json'=>$data,'headers'=>$this->headers]);
+            $response = $request->getBody();
+        } catch (ClientException $e) {
+            $response = Psr7\Message::toString($e->getResponse());
+        }
+        return $response;
+    }
+
+    /**
+     * @param $url
+     * @param $data
+     * @return StreamInterface|string
+     * @throws GuzzleException
+     */
+    public function putRequests($url,$data='')
+    {
+        try {
+            $request = $this->client->request('PUT',$this->url.$url,['json'=>$data,'headers'=>$this->headers]);
+            $response = $request->getBody();
+        } catch (ClientException $e) {
+            $response = Psr7\Message::toString($e->getResponse());
+        }
+        return $response;
+    }
+
 }
